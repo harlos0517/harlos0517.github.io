5b3963f674babb74b44bcc404dd2092dcdfdc9f6
DeemoHarlos <cces4415@gmail.com>
2017-10-12 18:48:58 +0800

    some image onload event

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-var headpic = document.getElementById('headpic')
-function showHeadpic(){
-	headpic.style.opacity = 1
-	headpic.style.transform = 'scale(1)'
+function $(a){
+	var e = document.querySelectorAll(a)
+	if (e.length == 1) return e[0]
+	else return e
 }
-window.onload = showHeadpic
+function loaded(e,f){
+	e.addEventListener('load',f)
+}
+function showHeadpic(){
+	$('#headpic').style.opacity = 1
+	$('#headpic').style.transform = 'scale(1)'
+}
+loaded($('#headpic>img'),showHeadpic)
+var bgs = $('#main-background>.bg')
+loaded(window,()=>{
+	bgs[0].style.opacity = 1
+})
