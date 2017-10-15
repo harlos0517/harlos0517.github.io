568270ec4fb2d4d516772977c7c0de1c04304bb0
DeemoHarlos <cces4415@gmail.com>
2017-10-16 06:58:55 +0800

    Add log message with full log info animation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*General Function Declaration*/
 function $(a){
 	var e = document.querySelectorAll(a)
 	if (e.length == 1) return e[0]
@@ -6,12 +7,35 @@ function $(a){
 function loaded(e,f){
 	e.addEventListener('load',f)
 }
+function clicked(e,f){
+	e.addEventListener('click',f)
+}
+
+/*headpic loaded animation*/
 function showHeadpic(){
 	$('#headpic').style.opacity = 1
 	$('#headpic').style.transform = 'scale(1)'
 }
 loaded($('#headpic>img'),showHeadpic)
+
+/*background loaded animation*/
 var bgs = $('#main-background>.bg')
 loaded(window,()=>{
 	bgs[0].style.opacity = 1
+})
+
+/*loginfo click animtation*/
+var logInfoShown = false
+clicked($('#loginfo'),()=>{
+	var log = $('#log')
+	if(!logInfoShown){
+		log.style.flex = '0 0 300px'
+		log.style.margin = '20px 20px 50px 20px'
+		logInfoShown = true
+	}
+	else{
+		log.style.flex = '0 0 0'
+		log.style.margin = '20px 0 50px 0'
+		logInfoShown = false
+	}
 })
