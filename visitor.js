507882c90e108d1a6c80133a85f5364015daffe6
DeemoHarlos <cces4415@gmail.com>
2017-11-01 02:10:36 +0800

    add visitor counter

---
+++ visitor.js
@@ -0,0 +1,9 @@
+var req = new XMLHttpRequest()
+var server = 'http://deemo.pw:11518'
+req.open('POST',server)
+req.setRequestHeader('Content-Type','application/X-www-form-urlencoded')
+req.send('website=deemo.life&userAgent=' + window.navigator.userAgent)
+req.onreadystatechange = function () {
+  if(req.readyState === XMLHttpRequest.DONE)
+    console.log(server + ' responded with status ' + req.status)
+}
