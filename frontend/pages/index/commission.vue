10146e40c69a849e2144189dbcc508bd3f37c9a7
DeemoHarlos <cces4415@gmail.com>
2022-08-29 17:21:20 +0800

    whole new version

---
+++ frontend/pages/index/commission.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  div.text-center Under construction...
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style lang="sass" scoped></style>
