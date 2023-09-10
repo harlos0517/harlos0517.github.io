580af09043b436d06d7f2afa47c1b4442ac3862b
DeemoHarlos <cces4415@gmail.com>
2023-09-11 00:30:39 +0800

    build fix

---
+++ docs/_nuxt/89086d6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{438:function(t,e,n){"use strict";n.r(e);var c=n(144),o=Object(c.b)({setup:function(){return{}}}),r=n(100),component=Object(r.a)(o,(function(){var t=this,e=t._self._c;t._self._setupProxy;return e("div",{staticClass:"text-center"},[t._v(t._s(t.$t("todo")))])}),[],!1,null,"1e84cf56",null);e.default=component.exports}}]);
