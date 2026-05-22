f1f1df306bd375fc3ce859013056b39c3aff7b9e
Harlos <cces4415@gmail.com>
2026-05-22 16:32:08 +0800

    update default works and fix comission default item

--- v5/deploy.sh
+++ v5/deploy.sh
@@ -2,6 +2,6 @@ pnpm build &&
 mkdir ../temp &&
 mv ../docs/v1 ../docs/v2 ../docs/v3 ../docs/v4 ../docs/CNAME ../docs/.nojekyll ../temp/ &&
 rm -rf ../docs/* &&
-cp ../build/* ../docs/ -r &&
+cp -r ../build/* ../docs/ &&
 mv ../temp/* ../temp/.nojekyll ../docs/ &&
 rm -rf ../temp;
